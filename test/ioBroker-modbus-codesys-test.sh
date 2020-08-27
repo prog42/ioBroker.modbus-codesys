2ffea477d9d05f182c858cff0acab648361d0d2f
prog42 <b.code@amb30.de>
2020-08-27 23:24:12 +0200

    added test codesys project and shell script

---
+++ test/ioBroker-modbus-codesys-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SYMBOL_XML_FILE=$(ls -Art *.SYM_XML 2> /dev/null | tail -n 1)
+
+if [ -z "$SYMBOL_XML_FILE" ]; then
+  SYMBOL_XML_FILE=$(ls -Art ../*.SYM_XML 2> /dev/null | tail -n 1)
+fi
+
+if [ -z "$SYMBOL_XML_FILE" ]; then
+  echo "could not find symbol-xml"
+  exit
+fi
+
+BASE=$(basename $SYMBOL_XML_FILE)
+
+../target/debug/ioBroker-modbus-codesys-convert -S $SYMBOL_XML_FILE -F iob
+
+cp inputs-out.csv $BASE-inputs-out.csv
+cp holdings-out.csv $BASE-holdings-out.csv
